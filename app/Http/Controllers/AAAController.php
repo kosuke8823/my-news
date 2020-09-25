d58bb8b9adb3a4fb37016cafce8e6960eb100bfd
kosuke8823 <kosuke.urata@rakuten-sec.co.jp>
2020-09-25 18:51:20 +0900

    first commit

---
+++ app/Http/Controllers/AAAController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AAAController extends Controller
+{
+    public function bbb()
+    {
+        return "aaa";
+    } 
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+Route::get('xxx', 'AAAController@add');
